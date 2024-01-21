f91e9b3dd774f67ce62d465a33d7f114f8058db6
sreeragvn <sreerag.office@gmail.com>
2024-01-21 23:19:51 +0100

    carsii cl sequence module works

--- data_utils/build_data_handler.py
+++ data_utils/build_data_handler.py
@@ -12,21 +12,3 @@ def build_data_handler():
             return getattr(module, attr)()
     else:
         raise NotImplementedError('DataHandler Class {} is not defined in {}'.format(datahandler_name, module_path))
-
-# def build_data_handler():
-#     if configs['data']['type'] == 'general_cf':
-#         data_handler = DataHandlerGeneralCF()
-#     elif configs['data']['type'] == 'sequential':
-#         data_handler = DataHandlerSequential()
-#     elif configs['data']['type'] == 'multi_behavior':
-#         data_handler = DataHandlerMultiBehavior()
-#     elif configs['data']['type'] == 'social':
-#         data_handler = DataHandlerSocial()
-#     elif configs['data']['type'] == 'kg':
-#         data_handler = DataHandlerKG()
-#     else:
-#         raise NotImplementedError
-
-#     return data_handler
-
-
