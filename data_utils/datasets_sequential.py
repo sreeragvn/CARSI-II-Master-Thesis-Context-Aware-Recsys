008b0ae6103f8f64dbab1bedd3d87223e4f931ae
sreeragvn <sreerag.office@gmail.com>
2024-01-23 13:12:19 +0100

    time delta reached till trainer

--- data_utils/datasets_sequential.py
+++ data_utils/datasets_sequential.py
@@ -18,14 +18,18 @@ class SequentialDataset(data.Dataset):
         self.max_seq_len = configs['model']['max_seq_len']
         self.user_history_lists = {user: seq for user,
                                    seq in zip(user_seqs["uid"], user_seqs["item_seq"])}
+        self.user_history_time_delta_lists = {user: time_delta for user,
+                                   time_delta in zip(user_seqs["uid"], user_seqs["time_delta"])}
         if user_seqs_aug is not None:
             self.uids = user_seqs_aug["uid"]
             self.seqs = user_seqs_aug["item_seq"]
             self.last_items = user_seqs_aug["item_id"]
+            self.time_delta = user_seqs_aug["time_delta"]
         else:
             self.uids = user_seqs["uid"]
             self.seqs = user_seqs["item_seq"]
             self.last_items = user_seqs["item_id"]
+            self.time_delta = user_seqs["time_delta"]
 
         if mode == 'test':
             self.test_users = self.uids
@@ -39,6 +43,14 @@ class SequentialDataset(data.Dataset):
             # pad at the head
             seq = [0] * (self.max_seq_len - len(seq)) + seq
         return seq
+    
+    def _pad_time_delta(self, seq, time_delta):
+        if len(seq) >= self.max_seq_len:
+            time_delta = time_delta[-self.max_seq_len:]
+        else:
+            # pad at the head
+            time_delta = [0] * (self.max_seq_len - len(seq)) + time_delta
+        return time_delta
 
     def sample_negs(self):
         if 'neg_samp' in configs['data'] and configs['data']['neg_samp']:
@@ -60,7 +72,8 @@ class SequentialDataset(data.Dataset):
 
     def __getitem__(self, idx):
         seq_i = self.seqs[idx]
+        time_delta_i = self.time_delta[idx]
         if self.mode == 'train' and 'neg_samp' in configs['data'] and configs['data']['neg_samp']:
-            return self.uids[idx], torch.LongTensor(self._pad_seq(seq_i)), self.last_items[idx], self.negs[idx]
+            return self.uids[idx], torch.LongTensor(self._pad_seq(seq_i)), self.last_items[idx], torch.LongTensor(self._pad_time_delta(seq_i, time_delta_i)), self.negs[idx]
         else:
-            return self.uids[idx], torch.LongTensor(self._pad_seq(seq_i)), self.last_items[idx]
+            return self.uids[idx], torch.LongTensor(self._pad_seq(seq_i)), self.last_items[idx], torch.LongTensor(self._pad_time_delta(seq_i, time_delta_i))
